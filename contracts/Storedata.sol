997abcd3f581965ca64f6e3ddaa1360057cac939
Mordern123 <eeric55tw@gmail.com>
2021-01-22 16:38:33 +0800

    可以儲存交易後金額

--- contracts/Storedata.sol
+++ contracts/Storedata.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.4.22 <0.9.0;
 import "./UserManage.sol";
 
 //儲存交易者資料
-contract StoreStockinformation {
+contract StockManagement {
     
     UserManagement UM; //global
     // string _name;//哪位使用者
@@ -11,41 +11,36 @@ contract StoreStockinformation {
         UserManagement um = UserManagement(um_addr); //get contract
         UM = um;
     }
-
-    uint money = 2000000;
     
-    mapping(string => mapping(string => Txn)) public stock;
-    struct Txn{
+    mapping(string => mapping(string => stockTxn)) public stock;
+    struct stockTxn{
         string stock_Name;      //公司名稱
-        uint stock_Id;          //股票代號
         uint stock_NowPrice;    //股票價格
         uint stock_Number;      //股票數量
     }
     
-    mapping(string => MoneyTxn) public property;//帳號對到持有金額
-    struct MoneyTxn{
-        uint cash;
-        uint stock_value;
-    }
-    
     //儲存股票資料
-    function setStockId(string memory account, string memory stockId, string memory stock_Name, uint stock_Id, uint stock_NowPrice, uint stock_Number) public{
+    function setStockId(string memory account, string memory stockId, string memory stock_Name, uint stock_NowPrice, uint stock_Number) public{
         if(UM.userIsExist(account)) {
-            stock[account][stockId] = Txn(stock_Name, stock_Id, stock_NowPrice, stock_Number); 
+            stock[account][stockId] = stockTxn(stock_Name, stock_NowPrice, stock_Number); 
         }
     }
 
-    function getStockId(string memory account, string memory stockId) public view returns(string memory, uint, uint, uint){
-        Txn memory t = stock[account][stockId]; //txn mapping
-        return (t.stock_Name, t.stock_Id, t.stock_NowPrice, t.stock_Number);
+    function getStockId(string memory account, string memory stockId) public view returns(string memory, string memory, uint, uint){
+        stockTxn memory t = stock[account][stockId]; //txn mapping
+        return (t.stock_Name, stockId, t.stock_NowPrice, t.stock_Number);
     }
     
     //處理交易資料
-    function tradeStock(string memory account, string memory stockId, string memory stock_Name, uint stock_Id, uint stock_NowPrice, uint stock_Number) public{
-        Txn memory t1 = stock[account][stockId]; //txn mapping
-        //持有金額(原本就在)
-        //數量*價格 = 花費金額(傳入)
-        //持有金額 - 花費金額 = 現在持有金額(更新持有金額)
+    function tradeStock(string memory account, string memory stockId, string memory stock_Name, uint stock_NowPrice, uint stock_Number) public{
+        if(UM.userIsExist(account)) {
+            stock[account][stockId] = stockTxn(stock_Name, stock_NowPrice, stock_Number); 
+            uint expenseStock = stock_NowPrice * stock_Number * 1000;       //數量*價格 = 花費金額(傳入)
+            (uint cash, uint value) = UM.getUserMoney(account);             //拿到UserManage的cash跟value
+            cash = cash - expenseStock;     //買股票完後金額
+            UM.updateUserMoney(account, cash, value);
+        }
+
         //(如果持有金額 < 花費金額 => 交易失敗)
         //購買價格
         //購買股票代號、企業名稱、股票數量
--- contracts/UserManage.sol
+++ contracts/UserManage.sol
@@ -5,29 +5,39 @@ contract UserManagement {
     mapping(string => userinformation) public person;
     
     struct userinformation{
-        string _username;       //使用者名稱
-        string _userpassword;   //使用者密碼
-        string _usergrade;      //使用者學年
-        string _usermajor;      //使用者科系
+        string user_name;       //使用者名稱
+        string user_password;   //使用者密碼
+        string user_grade;      //使用者學年
+        string user_major;      //使用者科系
+        uint user_cash;         //使用者現金
+        uint user_stockvalue;   //使用者股票價值
     }
     //使用者註冊
-    function setuser(string memory _useraccount, string memory _username, string memory _userpassword, string memory _usergrade, string memory _usermajor) public{
-        person[_useraccount] = (userinformation(_username, _userpassword, _usergrade, _usermajor));     
+    function setUser(string memory _useraccount, string memory user_name, string memory user_password, string memory user_grade, string memory user_major) public{
+        person[_useraccount] = (userinformation(user_name, user_password, user_grade, user_major, 2000000, 0));     
     }
     //觀看註冊是否成功
-    function getuser(string memory _useraccount) public view returns(string memory, string memory, string memory, string memory, string memory){
+    function getUser(string memory _useraccount) public view returns(string memory, string memory, string memory, string memory, string memory, uint, uint){
         userinformation memory ui = person[_useraccount];
-        return (ui._username, _useraccount, ui._userpassword, ui._usergrade, ui._usermajor);
+        return (ui.user_name, _useraccount, ui.user_password, ui.user_grade, ui.user_major, ui.user_cash, ui.user_stockvalue);
     }
-    
-    function userIsExist(string memory _useraccount) public view returns(bool) {
-        return bytes(person[_useraccount]._username).length != 0;
+    //是否存在此帳號
+    function userIsExist(string memory _useraccount) public returns(bool) {
+        return bytes(person[_useraccount].user_name).length != 0;
     }
-    
+    //拿到使用者的錢
+    function getUserMoney(string memory _useraccount) public returns(uint, uint){
+        return (person[_useraccount].user_cash, person[_useraccount].user_stockvalue);
+    }
+    //更新使用者的錢
+    function updateUserMoney(string memory _useraccount, uint cash, uint stockvalue) public{
+        person[_useraccount].user_cash = cash;
+        person[_useraccount].user_stockvalue = stockvalue;
+    }
     int a=0;//測試登入機制
     //使用者登入
     function inputuser(string memory account, string memory password) public{
-        if(keccak256(abi.encodePacked(password)) == keccak256(abi.encodePacked(person[account]._userpassword))){
+        if(keccak256(abi.encodePacked(password)) == keccak256(abi.encodePacked(person[account].user_password))){
             //登入成功
             a=1;
         }
